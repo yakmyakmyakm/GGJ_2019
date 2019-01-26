a3d859cf29aadce212ca45abc8662dce8015166f
cameron <cameron.loui@gmail.com>
2019-01-25 23:17:33 -0800

    rename player

--- UnityProject/Assets/Scripts/PlayerObject.cs
+++ UnityProject/Assets/Scripts/PlayerObject.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerObject : MonoBehaviour
 {
     MoveToClickPoint moveToClickPoint;
     MoveableObject moveableObject;
