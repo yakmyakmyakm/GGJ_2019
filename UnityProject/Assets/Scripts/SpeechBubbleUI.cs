464ac21355bc4e825458df920397738cde0a2b01
cameron <cameron.loui@gmail.com>
2019-01-26 21:35:12 -0800

    fix distraction generator

---
+++ UnityProject/Assets/Scripts/SpeechBubbleUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeechBubbleUI : MonoBehaviour
+{
+    public Image image;
+    public Text text;
+
+    public void MoveUp()
+    {
+
+    }
+
+    public void DisplayText(CharacterType type, string text)
+    {
+        this.text.text = text;
+        
+        if(type == CharacterType.AI)
+        this.transform.localRotation = Quaternion.Euler(new Vector3(0,180,0));
+
+    }
+}
---
+++ UnityProject/Assets/Scripts/TalkingUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TalkingUI : MonoBehaviour
+{
+    public SpeechBubbleUI speechBubble;
+    List<SpeechBubbleUI> bubbles = new List<SpeechBubbleUI>();
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    void Start()
+    {
+        Hide();
+        Initalize();
+    }
+
+    public void Initalize()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject o = Instantiate(speechBubble.gameObject) as GameObject;
+            bubbles.Add(o.GetComponent<SpeechBubbleUI>());
+        }
+    }
+}
