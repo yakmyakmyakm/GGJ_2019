fb26518740d21882d37853a984deddcc5c933f48
cameron <cameron.loui@gmail.com>
2019-01-26 22:46:47 -0800

    speech bubble ui

--- UnityProject/Assets/Scripts/TalkingUI.cs
+++ UnityProject/Assets/Scripts/TalkingUI.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[System.Serializable]
 public class TalkingUI : MonoBehaviour
 {
     public SpeechBubbleUI speechBubble;
     List<SpeechBubbleUI> bubbles = new List<SpeechBubbleUI>();
 
-    List<SpeechBubbleUI> displayedBubbles = new List<SpeechBubbleUI>();
+    public List<SpeechBubbleUI> displayedBubbles = new List<SpeechBubbleUI>();
+
+    int index;
+    Vector2 startPos = new Vector2(0, 0);
+
+    int totalItems = 20;
 
     public void Show()
     {
+        Initalize();
         this.gameObject.SetActive(true);
     }
 
@@ -20,65 +27,55 @@ public class TalkingUI : MonoBehaviour
         this.gameObject.SetActive(false);
     }
 
-    void Start()
-    {
-        //Hide();
-        Initalize();
-    }
-
-
-    int index;
-    Vector2 startPos = new Vector2(0,0);
-
     public void Initalize()
     {
-        for (int i = 0; i < 20; i++)
+        foreach (SpeechBubbleUI b in bubbles)
+        {
+            DestroyImmediate(b.gameObject);
+        }
+
+        bubbles.Clear();
+        displayedBubbles.Clear();
+        index = 0;
+
+        for (int i = 0; i < totalItems; i++)
         {
             GameObject o = Instantiate(speechBubble.gameObject, this.transform) as GameObject;
-            o.GetComponent<RectTransform>().position = startPos;
+            o.GetComponent<RectTransform>().localPosition = startPos;
             bubbles.Add(o.GetComponent<SpeechBubbleUI>());
         }
-
-        
-        
-        // ShowBubble(CharacterType.PLAYER, "3");
     }
-
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            ShowBubble(CharacterType.AI, "1");
-        }
-
-        if(Input.GetKeyDown(KeyCode.G))
-        {
-            ShowBubble(CharacterType.PLAYER, "2");
-        }
-    }
-
-
 
     public void ShowBubble(CharacterType type, string text)
     {
         SpeechBubbleUI bubble = bubbles[index];
         bubble.DisplayText(type, text);
-        
+
         index++;
-        if(index > 20)
+        if (index >= totalItems)
         {
             index = 0;
         }
 
+        int displayCount = 0;
+
         //move rest up
-        foreach(SpeechBubbleUI displayed in displayedBubbles)
+        foreach (SpeechBubbleUI displayed in displayedBubbles)
         {
             displayed.MoveUp();
+            displayCount++;
         }
+
         bubble.Show();
         displayedBubbles.Add(bubble);
 
+        if (displayCount > 4)
+        {
+            //begin fade out of top
+            displayedBubbles[0].GetComponent<RectTransform>().localPosition = Vector3.zero;
+            //Debug.Log(displayedBubbles[0].GetComponent<RectTransform>().localPosition);
+            displayedBubbles[0].SetAlpha(0);
+            displayedBubbles.RemoveAt(0);
+        }
     }
-
-
 }
