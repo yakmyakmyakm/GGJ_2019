8e40f58327f2211d888ab8a5092940ebc2668665
Colin Creitz <ccreitz@fb.com>
2019-01-26 22:00:06 -0800

    Added bedsnoop, startfiredistraction

---
+++ UnityProject/Assets/Scripts/BedSnoop.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class BedSnoop : Snoop
+{
+    public void StartSnoop()
+    {
+        base.StartSnoop();
+    }
+
+    public override void EndSnoop() {
+        TalkingManager.instance.AddSpeechData(
+            CharacterType.EVIDENCE,
+            "Under the pillow, there are hundreds, perhaps even thousands, of teeth.",
+            GameManager.DEFAULT_DIALOG_DURATION
+        );
+        TalkingManager.instance.AddSpeechData(
+            CharacterType.EVIDENCE,
+            "How long has your friend been living with devastating, dread scurvy?",
+            GameManager.DEFAULT_DIALOG_DURATION
+        );
+        TalkingManager.instance.AddSpeechData(
+            CharacterType.EVIDENCE,
+            "Are they a shark? This seems extreme, even by shark standards.",
+            GameManager.DEFAULT_DIALOG_DURATION
+        );
+
+
+        GameManager.learned.Add("How you've lost more teeth than I've even had in my life!");
+        GameManager.score += item.score;
+        if (MyFriend.Instance.IsWatchful()) {
+            TalkingManager.instance.AddSpeechData(
+                CharacterType.AI,
+                "So, you've found my little collection. I guess you have a lot of questions.",
+                GameManager.DEFAULT_DIALOG_DURATION
+            );
+            TalkingManager.instance.AddSpeechData(
+                CharacterType.PLAYER,
+                "I thought we were close! But you never told me about your life at sea.",
+                GameManager.DEFAULT_DIALOG_DURATION
+            );
+            }
+          
+        GameManager.instance.ComposeLitany();
+
+        bool isGood = GameManager.instance.RollFinalOutcome();
+
+        if(isGood) {
+            TalkingManager.instance.AddSpeechData(
+                CharacterType.AI,
+                "How did you know? Was it the shark-like quantity of teeth I've lost?",
+                GameManager.DEFAULT_DIALOG_DURATION
+            );
+            TalkingManager.instance.AddSpeechData(
+                CharacterType.PLAYER,
+                "And the scurvy!",
+                GameManager.DEFAULT_DIALOG_DURATION
+            );
+            TalkingManager.instance.AddSpeechData(
+                CharacterType.PLAYER,
+                "Yarr. It be a long, sad tale. Never had I had a mate to tell.",
+                GameManager.DEFAULT_DIALOG_DURATION
+            );
+
+            GameManager.instance.EndGame(true);
+        }
+        else {
+            TalkingManager.instance.AddSpeechData(
+                CharacterType.AI,
+                "In truth, I fear and hate the sea, ever since I had scurvy as a child.",
+                GameManager.DEFAULT_DIALOG_DURATION
+            );
+            TalkingManager.instance.AddSpeechData(
+                CharacterType.AI,
+                "But I do like teeth. And you have teeth... for another minute or two.",
+                GameManager.DEFAULT_DIALOG_DURATION
+            );
+            GameManager.instance.EndGame(false);
+        }
+
+        base.EndSnoop();
+    }
+
+    void Awake()
+    {
+        name = "bed";
+        Duration = 2.0f + Random.Range(0.0f, 1.0f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ UnityProject/Assets/Scripts/StartFireDistraction.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartFireDistraction : Distraction
+{
+    void Awake()
+    {
+        name = "startfire";
+        Duration = 4.0f + Random.Range(0.0f, 4.0f);
+    }
+
+    public override void StartPlayerDistraction()
+    {
+        base.StartPlayerDistraction();
+
+        TalkingManager.instance.AddSpeechData(
+            CharacterType.EVIDENCE,
+            "I bet that jerk Trevor left those logs there. He's always doing that.",
+            GameManager.DEFAULT_DIALOG_DURATION
+        );
+        TalkingManager.instance.AddSpeechData(
+            CharacterType.AI,
+            "What the heck! I don't even have matches in my house. Not anymore.",
+            GameManager.DEFAULT_DIALOG_DURATION
+        );
+
+        //AI can begin moving
+        //base.StartAIDistraction();
+    }
+
+    //AI calls when compelte fixing
+    public override void EndDistraction()
+    {
+        TalkingManager.instance.AddSpeechData(
+            CharacterType.AI,
+            "Why would you even do that? Those logs were decorative.",
+            GameManager.DEFAULT_DIALOG_DURATION
+        );
+
+        base.EndDistraction();
+
+        Debug.Log("complete log distractin");
+    }
+
+    // void BeginDistraction()
+    // {
+    //     this.kitten.StartDistraction();
+    //     MyFriend.Instance.EnqueueDistraction(this.kitten);
+}
